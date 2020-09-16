07664e19103a415f6acba7a3fdf915582f145fcd
Michael Klepac <mklepac90@gmail.com>
2020-09-16 22:38:42 +0000

    updated queries to be parameterized

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,18 +9,39 @@ const pool = new Pool({
 
 const myArgs = process.argv.slice(2);
 
+const cohortName = myArgs[0];
+const values = [`%${cohortName}%`];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${myArgs[0]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
-.then(res => {
+`;
+
+pool.query(queryString, values).then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 }).catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT DISTINCT(teachers.name) as teacher, cohorts.name as cohort
+// FROM assistance_requests
+// JOIN teachers ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON students.cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${myArgs[0]}%'
+// ORDER BY teacher;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
+
+
